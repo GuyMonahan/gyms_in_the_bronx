26f6ac30dcedce289fc5b08b4539c9b324edf016
Guy M <73130490+GuyMonahan@users.noreply.github.com>
2021-01-08 11:30:38 -0500

    Delete keys.py

--- Coffee Shop/keys.py
+++
@@ -1,8 +0,0 @@
-api_key='TfjmB8X5lfGFdM1Q-mfxbIG3gdsUSC_cB3epy-RiY1aBHUxKmgr9mhgASDJOUfZgSwBnq-ijmnWzLTmPZKkIBvH-DSFNinNcCEklcmSEAsl0pHmSI9smMe-BW03zX3Yx'
-
-client_id = 'xAM4Jh1qWlMNFi9iRMQ74g'
-
-
-#'TfjmB8X5lfGFdM1Q-mfxbIG3gdsUSC_cB3epy-RiY1aBHUxKmgr9mhgASDJOUfZgSwBnq-ijmnWzLTmPZKkIBvH-DSFNinNcCEklcmSEAsl0pHmSI9smMe-BW03zX3Yx'
-
-#'4xVh2R6L8UUhvHExeDY0PleqYWWvwBf_2YSXBs7tcJ4Fhlb_Ukti8ubodJ_KN7GIiAn_RSTI_tFwrXYpksOnzsfEQ1n1viib67dzyUStHa6t1hvIM_lWY5uk6GzzX3Yx'
